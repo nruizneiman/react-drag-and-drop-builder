655b9e1f70024f5c83f41a4452ed9bb40d10720b
Nicolás Ruiz Neiman <nicolasruizneiman@gmail.com>
2021-06-23 22:10:45 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Builder from './components/Builder';
+
+function App() {
+  return (
+    <div>
+      <Builder />
+    </div>
+  );
+}
+
+export default App;
